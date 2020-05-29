f06dc68818150190c06dfe8eb59b0f935f496115
kumarlalapet <muthulalapet@gmail.com>
2020-05-29 16:09:13 -0700

    Azure initial setup

--- imply-manager-on-azure/code.sh
+++ imply-manager-on-azure/code.sh
@@ -1,1 +1,7 @@
-(curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash); curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl; chmod +x ./kubectl; sudo mv ./kubectl /usr/local/bin/kubectl; curl -L0 https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz; tar -zxvf helm-v3.2.1-linux-amd64.tar.gz; mv linux-amd64/helm /usr/local/bin/helm
+(curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash);
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl;
+chmod +x ./kubectl;
+sudo mv ./kubectl /usr/local/bin/kubectl;
+curl -LO https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz;
+tar -xvf helm-v3.2.1-linux-amd64.tar.gz;
+mv linux-amd64/helm /usr/local/bin/
