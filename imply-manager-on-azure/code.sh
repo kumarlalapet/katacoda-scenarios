da77c2ac3a7ae67ca740f58bf36b0f2015b46284
kumarlalapet <muthulalapet@gmail.com>
2020-05-29 16:01:17 -0700

    Azure initial setup

--- imply-manager-on-azure/code.sh
+++ imply-manager-on-azure/code.sh
@@ -1,1 +1,1 @@
-(curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash)
+(curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash); curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl; chmod +x ./kubectl; sudo mv ./kubectl /usr/local/bin/kubectl; curl -L0 https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz; tar -zxvf helm-v3.2.1-linux-amd64.tar.gz; mv linux-amd64/helm /usr/local/bin/helm
--- imply-manager-on-azure/intro.md
+++ imply-manager-on-azure/intro.md
@@ -2,6 +2,11 @@ This scenario teaches you how to install Imply cluster on Kubernetes using Imply
 
 Azure Kubernetes Cluster (AKS) will be created as part of this scenario.
 
+The scenario installs the following tools
+
+- Azure cli 
+- kubectl cli
+- Helm
 
 #### NOTE - Please remember to delete the AKS cluster.
 
